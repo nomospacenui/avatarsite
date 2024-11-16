ce21c7e9fb173d4b737f7d03fe5ca67388de531e
nomospacenui <nomospacemail@gmail.com>
2024-11-17 01:04:09 +0800

    ADD: Page layout styling and setup

---
+++ build/scripts/page.js
@@ -0,0 +1,27 @@
+
+class Page {
+    constructor() {
+        this.create_navigation()
+    }
+
+    create_navigation() {
+        var navigation_bar = document.getElementById("contentbox-header")
+
+        const nav_items = ["Home", "Avatar", "Forums", "Shops", "Games"]
+
+        for (var i = 0 ; i < nav_items.length ; ++i) {
+            
+            var button = document.createElement("button")
+            button.innerHTML = nav_items[i]
+            button.className = "nav-button"
+
+            if (i == 0) {
+                button.style.borderLeft = "1px solid #7D528C"
+            }
+
+            navigation_bar.appendChild(button)
+        }
+    }
+}
+
+new Page()
