c035c6c6682425df6ae1528a9cf59e5d83f9b184
nomospacenui <nomospacemail@gmail.com>
2024-12-12 02:09:04 +0800

    UPDATE: alignment and font size buttons

--- build/scripts/forums/create_thread.js
+++ build/scripts/forums/create_thread.js
@@ -47,11 +47,47 @@ class CreateThread {
         underline_button.onclick = function() { modifier_button_click(input_box, "<u>", "</u>") }
         buttons_menu.append(underline_button)
         
-        var strikethrough_button = document.createElement("button")
-        strikethrough_button.className = "text-buttons"
-        strikethrough_button.innerHTML = "<s>S</s>"
-        strikethrough_button.onclick = function() { modifier_button_click(input_box, "<s>", "</s>") }
-        buttons_menu.append(strikethrough_button)
+        var left_align_button = document.createElement("button")
+        left_align_button.className = "text-buttons"
+        left_align_button.innerHTML = "L"
+        left_align_button.onclick = function() { modifier_button_click(input_box, "<p style='text-align: left'>", "</p>") }
+        buttons_menu.append(left_align_button)
+        
+        var center_align_button = document.createElement("button")
+        center_align_button.className = "text-buttons"
+        center_align_button.innerHTML = "C"
+        center_align_button.onclick = function() { modifier_button_click(input_box, "<p style='text-align: center'>", "</p>") }
+        buttons_menu.append(center_align_button)
+        
+        var right_align_button = document.createElement("button")
+        right_align_button.className = "text-buttons"
+        right_align_button.innerHTML = "R"
+        right_align_button.onclick = function() { modifier_button_click(input_box, "<p style='text-align: right'>", "</p>") }
+        buttons_menu.append(right_align_button)
+        
+        var font_size_dropdown = document.createElement("button")
+        font_size_dropdown.className = "text-buttons-dropdown"
+        font_size_dropdown.innerHTML = "font size"
+        
+        var font_size_dropdown_content = document.createElement("div")
+        font_size_dropdown_content.className = "text-buttons-content"
+        font_size_dropdown.onclick = function() { 
+            font_size_dropdown_content.style.visibility = (font_size_dropdown_content.style.visibility == "visible") ?
+                "hidden" :
+                "visible"
+        }
+        
+        for (var i = 10 ; i <= 24 ; i += 2) {
+            var font_size = document.createElement("p")
+            font_size.innerHTML = i
+            font_size.className = "text-buttons-content-entry"
+            const c_i = i
+            font_size.onclick = function() { modifier_button_click(input_box, "<p style='font-size:" + c_i + "'>", "</p>") }
+            font_size_dropdown_content.append(font_size)
+        }
+
+        font_size_dropdown.append(font_size_dropdown_content)
+        buttons_menu.append(font_size_dropdown)
 
         return buttons_menu
     }
@@ -62,6 +98,10 @@ function modifier_button_click(input_box, modifier_start, modifier_end) {
     const mid_str = input_box.value.substring(input_box.selectionStart, input_box.selectionEnd)
     const aft_str = input_box.value.substring(input_box.selectionEnd, input_box.value.length)
     input_box.value = bef_str + modifier_start + mid_str + modifier_end + aft_str;
+    
+    input_box.focus()
+    var end_insert_pos = (bef_str + modifier_start + mid_str + modifier_end).length
+    input_box.selectionEnd = end_insert_pos
 }
 
 export default CreateThread
